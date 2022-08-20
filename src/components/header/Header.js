432a466cad75a90f6e5934bd800a366c0562b757
Anish <ghogareanish@gmail.com>
2022-08-20 21:22:29 +0530

    done with the header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,14 +10,17 @@ const Header = () => {
         <div className="header__container">
       <h5>Hello I'm</h5>
       <h1>Anish Ghogare</h1>
-      <h5 className="text-light">Software Engineer</h5>
+      <h5 className="text-light">Software Developer</h5>
       <Cta/>
       <HeaderSocials/>
-      
-      <a href="#contact" className="scroll__down">Scroll Down</a>
       <div className="me">
           {/* <img src={me} alt="me" /> */}
       </div>
+      
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+      
+      
+      
       
       </div>
     </header>
